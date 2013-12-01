98cb76b5dd01296f485383634955f015e6af470c
Alex Howington <alex.howington@gmail.com>
2013-11-30 23:04:47 -0700

    Move secret token into ENV variable

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-EasyMaps::Application.config.secret_token = '68a49eb4ef04ac72fb9b0448b2f10fb8e528bc49d66cb3fb132691af85e01208a583b98ba7c807a768446f6a0ea86f5e2f6469ec1f8ccd762cce3626c7997bd3'
+EasyMaps::Application.config.secret_token = ENV['SECRET_TOKEN']
